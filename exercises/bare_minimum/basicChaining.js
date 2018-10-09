9b2a5228e3ca183e80b57dafadb1b63c6df802e6
amieguo <amieguo@gmail.com>
2018-10-09 16:58:53 -0700

    finished all bare

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -3,18 +3,38 @@
  * (1) reads a GitHub username from a `readFilePath`
  *     (the username will be the first line of the file)
  * (2) then, sends a request to the GitHub API for the user's profile
- * (3) then, writes the JSON response of the API to `writeFilePath`
- *
+ * (3) then, writes the JSON response of the API to `writeFilePath` *
  * HINT: We exported some similar promise-returning functions in previous exercises
+ */
+ 
+ /*
+ //from previous sprint
+ module.exports = {
+  getGitHubProfileAsync: getGitHubProfileAsync,
+  generateRandomTokenAsync: generateRandomTokenAsync,
+  readFileAndMakeItFunnyAsync: readFileAndMakeItFunnyAsync
+};
  */
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
+var readLine = require('./promiseConstructor.js');
+var getProfile = require('./promisification.js')
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  
+  
+  return readLine.pluckFirstLineFromFileAsync(readFilePath) 
+  .then (function(userName) {
+    return getProfile.getGitHubProfileAsync(userName)
+  })
+  .then (function(body) {
+    console.log(body)
+    return fs.writeFileSync(writeFilePath, JSON.stringify(body))
+  })
+  // .catch();
+  
 };
 
 // Export these functions so we can test them
--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -52,11 +52,27 @@ var readFileAndMakeItFunny = function(filePath, callback) {
       })
       .join('\n');
 
-    callback(funnyFile);
+    callback(null, funnyFile);
   });
 };
-
 var readFileAndMakeItFunnyAsync = Promise.promisify(readFileAndMakeItFunny);
+
+
+
+// var readFileAndMakeItFunnyAsync = function (filePath) {
+//   return new Promise ((res, rej)=>{
+//      fs.readFile(filePath, 'utf8', function(err, file) {
+//       if(err) {
+//         return rej(err)
+//       } 
+//       var funnyFile = file.split('\n')
+//       .map(function(line) {  return line + ' lol'}).join('\n');
+      
+//       res(funnyFile)  
+//     });
+//   })
+// }
+
 
 // Export these functions so we can test them and reuse them in later exercises
 module.exports = {
